ce06e5ce5bcdd5de3196959ec657919e96d53cfe
Aleksandar <milicoa97@gmail.com>
2022-12-12 08:38:09 +0100

    rewrite regex to import monkeys

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+#![allow(unused)]
 use std::{path::Path};
 use crate::utils::read_string;
-
+use regex::Regex;
 mod utils;
 mod puzzle;
 
 fn main() {
-    let path = Path::new("./puzzle_input/2022/11/description_input.txt");
+    /*let path = Path::new("./puzzle_input/2022/11/description_input.txt");
     let puzzle_input = read_string(path);
-    println!("{}", puzzle_input);
+    println!("{}", puzzle_input);*/
+    let puzzle_input = r#"Monkey 12:"#;
+    let re = Regex::new(
+        r#"Monkey (\d+):"#
+    ).expect("should be a valid regex!");
+    let result = re.captures(puzzle_input).unwrap();
+    let a = 5;
+    //println!("{}", result.())
 }
--- src/puzzle/monkey_in_the_middle_11.rs
+++ src/puzzle/monkey_in_the_middle_11.rs
@@ -24,13 +24,19 @@ pub fn parse_monkies(puzzle_input: &str) -> Vec<Monkey> {
     let re = Regex::new(
         r#"Monkey (\d+):
              Starting items: (\d+)
-             Operation: new = old (+|*) (\d+)
+             Operation: new = old (\+|\*) (\d+)
              Test: divisible by (\d+)
                If true: throw to monkey (\d+)
-               If false: throw to monkey (\d+)"#,
+               If false: throw to monkey (\d+)"#
     ).expect("should be a valid regex!");
 
-    re.captures_iter(puzzle_input).filter_map(|cap| {
+    let re = Regex::new(
+        r#"Monkey (\d+):"#,
+    ).expect("should be a valid regex!");
+    let result = re.captures(puzzle_input).unwrap();
+    
+
+    /*let result = re.captures_iter(puzzle_input).filter_map(|cap| {
         let a = cap;
         Some(Monkey {
                 id: 1,
@@ -43,7 +49,8 @@ pub fn parse_monkies(puzzle_input: &str) -> Vec<Monkey> {
                 test_false: 3
             })
         }
-    ).collect::<Vec<Monkey>>()
+    ).collect::<Vec<Monkey>>();*/
+    vec![]
 }
 
 pub fn play_game(input: String) -> () {
@@ -58,14 +65,19 @@ mod tests {
 
     #[test]
     fn test_monkey_parser() {
-        let monkey_text = "
-        Monkey 0:
-          Starting items: 79, 98
-          Operation: new = old * 19
-          Test: divisible by 23
-            If true: throw to monkey 2
-            If false: throw to monkey 3
-        ";
+        let a = 5;
+        let b = a + 3; 
+        let monkey_text = 
+        r#"Monkey 0:
+             Starting items: 79, 98
+              Operation: new = old * 19
+              Test: divisible by 23
+                If true: throw to monkey 2
+                If false: throw to monkey 3
+        "#;
+
+        let monkey_text = 
+        r#"Monkey 0:Monkey 0:"#;
         
         let result = parse_monkies(&monkey_text);
         //assert_eq!();
