f14ee4dda2f38a38d55de1a38a4eecc709f125cd
Aleksandar <milicoa97@gmail.com>
2022-12-23 09:59:09 +0100

    add converter test 2022_01

--- src/puzzle/calorie_counting_01.rs
+++ src/puzzle/calorie_counting_01.rs
@@ -6,7 +6,7 @@ struct Elf {
 
 impl fmt::Display for Elf {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Elf: {}", "test");
+        write!(f, "Elf: {}", "test")
     }
 }
 
@@ -16,22 +16,23 @@ fn play_game(){
 
 fn read_elf_items(input: &str) -> Vec<Elf> {
 
-    let elfs: Vec<String> = input.split("\n\n").collect::<String>();
+    let elfs: Vec<&str> = input.split("\n\n").collect();
 
-    return elfs.map( |each_elf:String| 
+    return elfs.iter().map( |each_elf | 
             Elf{
                 snacks : each_elf.split("\n").map(
-                    |each_snack:str| each_snack.parse::<i32>()
-                )        
+                    |each_snack:&str| each_snack.parse::<i32>().expect("not a valid int!")
+                ).collect()        
             }
-        )
+        ).collect::<Vec<_>>()
 
 }
 
-#[cfg(test)] 
+#[cfg(test)]
 mod tests {
     use super::*;
 
+    #[test]
     fn test_parser(){
         let input = "1000
         2000
@@ -46,8 +47,7 @@ mod tests {
         8000
         9000
         
-        10000";
+        10000".trim();
         let result = read_elf_items(input);  
-        println!("{}", result)
     }
 }
