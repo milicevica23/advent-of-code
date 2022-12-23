481cda0e55ea3ac7d2cd1facbef69905e53ba79f
Aleksandar <milicoa97@gmail.com>
2022-12-23 11:47:35 +0100

    finish 2022_01

--- src/puzzle/calorie_counting_01.rs
+++ src/puzzle/calorie_counting_01.rs
@@ -1,7 +1,13 @@
 use std::fmt;
-
+#[derive(Debug)]
 struct Elf {
     snacks : Vec<i32>
+}
+
+impl Elf {
+    fn calculate_total(&self) -> i32 {
+        self.snacks.iter().fold(0,|a, b| a + b)
+    }
 }
 
 impl fmt::Display for Elf {
@@ -21,7 +27,9 @@ fn read_elf_items(input: &str) -> Vec<Elf> {
     return elfs.iter().map( |each_elf | 
             Elf{
                 snacks : each_elf.split("\n").map(
-                    |each_snack:&str| each_snack.parse::<i32>().expect("not a valid int!")
+                    |each_snack:&str| {
+                    //println!("{}", each_snack);
+                    each_snack.trim().parse::<i32>().expect("not a valid int!")}
                 ).collect()        
             }
         ).collect::<Vec<_>>()
@@ -30,24 +38,24 @@ fn read_elf_items(input: &str) -> Vec<Elf> {
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use super::*;
 
     #[test]
     fn test_parser(){
-        let input = "1000
-        2000
-        3000
-        
-        4000
-        
-        5000
-        6000
-        
-        7000
-        8000
-        9000
-        
-        10000".trim();
-        let result = read_elf_items(input);  
+        let file_path =  "C:\\Users\\SFP1Z6L\\git\\advent-of-code\\puzzle_input\\2022\\01\\submit_input.txt";
+        let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+        let mut result = read_elf_items(&contents);  
+
+        let max_elf = result.iter().max_by_key(|a| a.calculate_total());
+        println!("{}",max_elf.expect("max_elf").calculate_total());
+
+        result.sort_by_key(|a| -a.calculate_total());
+
+        println!("{:#?}", result);
+        let max_three = result[0..3].iter().fold(0,|a, b| a + b.calculate_total());
+        println!("max three: {}",max_three);
     }
 }
